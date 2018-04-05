bb7279d335fb6ab5c12caf1dbf748c65396fac3b
João Figueiredo <lucalanca2@hotmail.com>
2018-04-05 13:49:42 +0200

    adds options parameter to regionsGet

--- src/CockpitSDK.js
+++ src/CockpitSDK.js
@@ -223,9 +223,12 @@ class CockpitSDK {
   }
 
   // @param {string} regionName
-  regionGet(regionName) {
+  // @param {Request} options
+  regionGet(regionName, options) {
     return this.fetchDataText(`${this.endpoints.regionsGet}${regionName}`, {
-      method: 'GET',
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: this.stringifyOptions(options),
     });
   }
 
