ad2550dd8d82b4377d5930439fde1627fc8055b9
Bruno Lourenco <brunnolou@gmail.com>
2018-12-20 11:21:51 +0000

    Fix image srcset and numbers array

--- src/tests/CockpitSDK.test.js
+++ src/tests/CockpitSDK.test.js
@@ -1,5 +1,23 @@
 import CockpitSDK from '../CockpitSDK';
 
+test('Expect Cockpit.image to return array of numbers', () => {
+  const cockpit = new CockpitSDK({
+    host: 'foo',
+    accessToken: 'bar',
+    lang: 'biz',
+  });
+  const imageOptions = [10, 20, 30];
+
+  const result = cockpit.image('bux', imageOptions);
+
+  expect(result).toBe(
+    '' +
+      'foo/api/cockpit/image?d=1&lang=biz&o=1&src=bux&token=bar&w=10 10w, ' +
+      'foo/api/cockpit/image?d=1&lang=biz&o=1&src=bux&token=bar&w=20 20w, ' +
+      'foo/api/cockpit/image?d=1&lang=biz&o=1&src=bux&token=bar&w=30 30w',
+  );
+});
+
 test('Expect Cockpit.image to return height', () => {
   const cockpit = new CockpitSDK({
     host: 'foo',
@@ -86,5 +104,32 @@ test('Expect Cockpit.image with array to return pixel ratio', () => {
     '' +
       'foo/api/cockpit/image?d=1&h=200&lang=biz&o=1&src=bux&token=bar&w=400 200w, ' +
       'foo/api/cockpit/image?d=1&h=20&lang=biz&o=1&src=bux&token=bar&w=40 20w',
+  );
+});
+
+test('Expect Cockpit.image with array to return different srcSet', () => {
+  const cockpit = new CockpitSDK({
+    host: 'foo',
+    accessToken: 'bar',
+    lang: 'biz',
+  });
+  const imageOptions1 = {
+    width: 200,
+    height: 100,
+    srcSet: '1x',
+  };
+
+  const imageOptions2 = {
+    ...imageOptions1,
+    pixelRatio: 2,
+    srcSet: '2x',
+  };
+
+  const result = cockpit.image('bux', [imageOptions1, imageOptions2]);
+
+  expect(result).toBe(
+    '' +
+      'foo/api/cockpit/image?d=1&h=100&lang=biz&o=1&src=bux&token=bar&w=200 1x, ' +
+      'foo/api/cockpit/image?d=1&h=200&lang=biz&o=1&src=bux&token=bar&w=400 2x',
   );
 });
